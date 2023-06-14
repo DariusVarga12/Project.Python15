1b310c6db603b2b5b5308222e268f800d9b2ab67
DARIUS <darius.varga95@yahoo.com>
2023-06-14 20:26:10 +0300

    Final Project Python

---
+++ PYTHON_FinalProject/Merov/urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for Merov project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from Merov.views import homepage
+from services.views import get_all_services, get_service
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", homepage),
+    path("services/", get_all_services),
+    path("appointment_details/", get_service),
+]
---
+++ PYTHON_FinalProject/services/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+services = [
+    {
+        "id": 1,
+        "name": "(SkinFade/TaperFade)MEN",
+        "price": 70.00
+    },
+
+    {
+        "id": 2,
+        "name": "(CUT&BEARD)MEN",
+        "price": 80.00
+    },
+
+    {
+        "id": 3,
+        "name": "LADIES(full pack) short / medium hair / long hair",
+        "price": 100.00
+    },
+]
+
+def get_all_services(request):
+    return render(request, "services.html", {
+        "services": services
+    })
+
+def get_service(request):
+    return render(request, "appointment_details.html")
+
+def services(request):
+    return render(request, 'services/services.html')
+
+def appointment_details(request):
+    return render(request, 'services/appointment_details.html')
+
+
+
